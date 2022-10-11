c76b38334b2d9014db4dc65877c5cd13bbf91915
ss <sisg12345@yahoo.co.jp>
2022-10-11 20:30:22 +0900

    add: datepickerコンポーネント追加
    1. datepickerのコンポーネントを実装

---
+++ frontend/src/components/Parts/DatePicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="datepicker">
+    <Datepicker :bootstrap-styling="true" :format="dateFormat" locale="jp" cancel-text="キャンセル" select-text="OK" v-model="dateTime"></Datepicker>
+  </div>
+</template>
+<script>
+import { ref } from 'vue'
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
+export default {
+  name: 'DatePicker',
+  components: {
+    Datepicker
+  },
+  setup() {
+    // 日付
+    const dateTime = ref(null)
+    // 日付フォーマット: yyyy/MM/dd HH:mm
+    const dateFormat = ref('yyyy/MM/dd HH:mm')
+
+    return {
+      dateTime,
+      dateFormat
+    }
+  }
+}
+</script>
