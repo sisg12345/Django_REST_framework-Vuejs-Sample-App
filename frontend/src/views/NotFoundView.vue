1746ea89e8e975b8ff8fd489202e74fa4a6e636b
ss <sisg12345@yahoo.co.jp>
2022-10-11 17:31:17 +0900

    add: NotFoundView追加
    1. NotFoundページ雛形追加

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="page-not-found" class="text-">
+    <h1>404 Page Not Found</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NotFoundView'
+}
+</script>
+<style scoped>
+#page-not-found {
+  text-align: center;
+}
+</style>
