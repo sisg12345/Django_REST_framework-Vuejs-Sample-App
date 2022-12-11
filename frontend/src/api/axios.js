ec2a88994249e3a99a15481ac03b39808f1952fd
ss <sisg12345@yahoo.co.jp>
2022-12-11 23:09:44 +0900

    add: axios共通処理を追加
    1. axios共通処理を追加

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,31 @@
 import axios from 'axios'
+import store from '@/store'
 
 // axios初期設定
-const service = axios.create({
+const api = axios.create({
   // ベースURL
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  baseURL: 'http://localhost:8000/',
+  // baseURL: process.env.VUE_APP_API_BASE_URL,
   // タイムアウト
   timeout: 5000,
   // ヘッダー
-  headers: {
+  headers: { 
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
   }
 })
 
 // リクエスト共通処理
-service.interceptors.request.use(
+api.interceptors.request.use(
   (request) => {
+    // アクセストークを取得
+    const accessToken = localStorage.getItem('accessToken')
+
+    // アクセストークンをリクエストヘッダーに追加
+    if (accessToken) {
+      request.headers.Authorization = 'Bearer ' + accessToken
+    }
+
     return request
   },
   (error) => {
@@ -22,13 +34,41 @@ service.interceptors.request.use(
 )
 
 // レスポンス共通処理
-service.interceptors.response.use(
+api.interceptors.response.use(
   (response) => {
     return response
   },
-  (error) => {
+  async (error) => {
+     // 元のリクエスト情報
+    const originalRequestConfig = error.config
+
+    // 認証エラーの場合
+    if (error.response && error.response.status === 401 && !originalRequestConfig._retry) {
+      // 現在のエラーレスポンスに対してのみ有効に設定: 無限ループ対策
+      originalRequestConfig._retry = true
+      // 元のリクエストヘッダー情報の再代入: 再代入しないとエラーになる
+      originalRequestConfig.headers = { ...originalRequestConfig.headers }
+      // アクセストークン取得
+      var token = await api.post('api/v1/token/refresh/', {
+        refresh: localStorage.getItem('refreshToken')
+      })
+
+      if (token.status === 200) {
+        // アクセストークンをローカルストレージに保存
+        store.commit('auth/updateAccessToken', token.data.access)
+        // 再送信リクエストヘッダーにアクセストークンをセット
+        originalRequestConfig.headers.Authorization = 'Bearer ' + localStorage.getItem('accessToken')
+        // アクセストークン更新前のリクエスト処理を再開
+        return api.request(originalRequestConfig)
+      } else {
+        // アクセストークンの更新に失敗した場合
+        // ログアウト処理を行いログインページへ遷移
+        store.dispatch('auth/logout')
+      }
+    }
+
     return Promise.reject(error)
   }
 )
 
-export default service
+export default api
