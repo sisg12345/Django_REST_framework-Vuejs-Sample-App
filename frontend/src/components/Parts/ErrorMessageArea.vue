21017a5e1124aab54a7e7b69fb3f9d0ee347f64e
ss <sisg12345@yahoo.co.jp>
2022-12-11 23:05:47 +0900

    add: エラーメッセージ表示用コンポーネントを追加
    1. エラーメッセージ表示用コンポーネントを追加

---
+++ frontend/src/components/Parts/ErrorMessageArea.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="errorMessage !== null">
+    {{ errorMessage }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="closeMessage">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ErrorMessageArea',
+  props: {
+    errorMessage: {
+      type: String,
+      require: true
+    }
+  },
+  setup(props, context) {
+    // エラーメッセージ閉じる
+    const closeMessage = () => {
+      // エラーメッセージを閉じる処理呼び出し
+      context.emit('closeMessage')
+    }
+
+    return {
+      closeMessage
+    }
+  }
+}
+</script>
