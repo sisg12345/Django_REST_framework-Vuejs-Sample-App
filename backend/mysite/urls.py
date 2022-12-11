c4bae88fb266defb813e44652f0e3a6c16a34374
ss <sisg12345@yahoo.co.jp>
2022-12-11 23:11:01 +0900

    add: swaggerを追加
    1. sawaggerを追加

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+"""Swagger UI"""
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Swagger API",
+        default_version='3.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-ui"),
+    # My application
+    path('api/v1/', include('user.urls')),
+    path('api/v1/', include('auth.urls')),
 ]
